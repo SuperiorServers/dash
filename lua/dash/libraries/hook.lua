50e8153bc9118dcf6da166724422a197a965e508
aStonedPenguin <aStonedPenguin@users.noreply.github.com>
2017-09-24 01:21:17 -0800

    Made hooks faster again

--- lua/dash/libraries/hook.lua
+++ lua/dash/libraries/hook.lua
@@ -10,7 +10,9 @@ local isfunction 	= isfunction
 local IsValid 		= IsValid
 
 local hook_callbacks = {}
-local hook_mapping = {}
+local hook_mapping 	 = {}
+local hook_counts 	 = {}
+local hooks_remap 	 = false
 
 function hook.GetTable() -- This function is now slow
 	return table.Copy(hook_mapping)
@@ -24,15 +26,31 @@ function hook.Call(name, gm, ...)
 	local callbacks = hook_callbacks[name]
 
 	if (callbacks ~= nil) then
-		for i = 1, #callbacks do
-			local v = callbacks[i]
-			if (v ~= nil) then
-				local a, b, c, d, e, f = v(...)
-				if (a ~= nil) then
-					return a, b, c, d, e, f
-				end
+		local count = hook_counts[name]
+		local i = 0
+		::start::
+		i = i + 1
+		local v = callbacks[i]
+		if (v ~= nil) then
+			local a, b, c, d, e, f = v(...)
+			if (a ~= nil) then
+				return a, b, c, d, e, f
 			end
 		end
+		if (i < count) then goto start end
+	end
+
+	if (hooks_remap) then
+		local count = 0
+		hook_callbacks[name] = {}
+		for i = 1, hook_counts[name] do
+			if (callbacks[i] ~= nil) then
+				count = count + 1
+				hook_callbacks[name][count] = callbacks[i]
+			end
+		end
+		hook_counts[name] = count
+		hooks_remap = false
 	end
 
 	if (not gm) then
@@ -55,18 +73,26 @@ end
 function hook.Remove(name, id)
 	local callbacks = hook_callbacks[name]
 
-	if (callbacks ~= nil) then
-		local callback = hook_mapping[name][id]
-		local count = #callbacks
+	if (not callbacks) then
+		return
+	end
 
-		for i = 1, count do
-			if (callbacks[i] == callback) then
-				for newi = i, count do
-					callbacks[newi] = callbacks[newi + 1]
-				end
-				hook_mapping[name][id] = nil
-				break
+	local callback = hook_mapping[name][id]
+
+	if (not callback) then
+		return
+	end
+
+	local count = #callbacks
+
+	for i = 1, count do
+		if (callbacks[i] == callback) then
+			for newi = i, count do
+				callbacks[newi] = callbacks[newi + 1]
 			end
+			hook_mapping[name][id] = nil
+			hook_counts[name] = hook_counts[name] - 1
+			return
 		end
 	end
 end
@@ -79,6 +105,10 @@ function hook.Add(name, id, callback)
 		id = debug_info(callback).short_src
 	end
 
+	if (not callback) then
+		return
+	end
+
 	if (hook_callbacks[name] == nil) then
 		hook_callbacks[name] = {}
 		hook_mapping[name] = {}
@@ -88,18 +118,21 @@ function hook.Add(name, id, callback)
 		hook_Remove(name, id) -- properly simulate hook overwrite behavior
 	end
 
+	hook_counts[name] = (hook_counts[name] or 0) + 1
+	local index = hook_counts[name]
+
 	if (not isstring(id)) then
 		local orig = callback
 		callback = function(...)
 			if IsValid(id) then
 				return orig(id, ...)
-			else
-				hook_Remove(name, id)
 			end
+			hook_mapping[name][id] = nil
+			hook_callbacks[name][index] = nil
+			hooks_remap = true
 		end
 	end
 
-	local index = #hook_callbacks[name] + 1
 	hook_callbacks[name][index] = callback
 	hook_mapping[name][id] = callback
 end

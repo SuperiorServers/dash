d31ba5c53af80b25bf6f8aa62e48005e348b35c8
Scott <scottd564@users.noreply.github.com>
2021-04-16 11:01:16 +0100

    Fixed WEP.__index value check for false props

--- lua/dash/extensions/weapon.lua
+++ lua/dash/extensions/weapon.lua
@@ -2,11 +2,11 @@ local WEAPON, ENTITY = FindMetaTable 'Weapon', FindMetaTable 'Entity'
 local GetTable = ENTITY.GetTable
 local GetOwner = ENTITY.GetOwner
 
-local garryisaretard = 'Owner'
+local ownerkey = 'Owner'
 function WEAPON:__index(key)
 	local value = WEAPON[key] or ENTITY[key] or GetTable(self)[key]
 
-	if value then return value end
+	if value ~= nil then return value end
 
-	if (key == garryisaretard) then return GetOwner(self) end
+	if (key == ownerkey) then return GetOwner(self) end
 end
